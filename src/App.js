5ba33d43c48217933a2a5e2c55b5630582d022d4
Erik Pehrers <erik.pehrers@jetshop.se>
2019-06-12 19:04:53 +0200

    progress bar calc and some styling

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ const AppContainer = styled.div`
   margin: 6rem auto;
   padding: 20px;
   max-width: 550px;
-  min-width: 500px;
   background: rgba(255, 255, 255, 0.9);
   box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
   border-radius: 5px;
--- src/Components/ProgressBar.js
+++ src/Components/ProgressBar.js
@@ -1,19 +1,47 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+const BarWrapper = styled.div`
+  height: 12px;
+  padding: 4px;
+  background: rgba(0, 0, 0, 0.05);
+  border-radius: 12px;
+  margin: 2rem;
+`;
+
+const Indicator = styled.span`
+  position: absolute;
+  top: -22px;
+  right: -18px;
+  font-size: 0.75rem;
+  color: rgba(0, 0, 0, 0.2);
+`;
+
+const Bar = styled.div`
+  width: ${p => p.width};
+  height: 100%;
+  background: rgba(17, 145, 207, 1);
+  transition: all 1s ease;
+  border-radius: 11px;
+  position: relative;
+`;
 
 const ProgressBar = ({ todos }) => {
   const calcBarLength = todos => {
-    let barLength = 0;
-    if (todos.length == 0) return;
+    let barLength = todos.length;
+    if (barLength === 0) return barLength;
     let totalCompleted = 0;
     todos.forEach(item => (item.completed ? totalCompleted++ : null));
-    let barLength = parseFloat(totalCompleted / todos.length);
-    return barLength.toString();
+    barLength = (totalCompleted / todos.length) * 100;
+    return barLength;
   };
 
   return (
-    <div>
-      <span>{calcBarLength(todos)} done</span>
-    </div>
+    <BarWrapper>
+      <Bar width={`${calcBarLength(todos)}%`}>
+        <Indicator>{`${parseInt(calcBarLength(todos)).toString()}%`}</Indicator>
+      </Bar>
+    </BarWrapper>
   );
 };
 
